d7983f98236c7ff7a6c82185bf930ab4602bb7e5
Ethan Brierley <ethanboxx@gmail.com>
2020-02-20 07:38:14 +0000

    Move styles out of `index.html`

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
           {
             loader: "file-loader",
             options: {
-              name: "css/[name].css"
+              name: "../css/[name].css"
             }
           },
           {
