d4b939716b77a8f457c9855079bafe553b3d7cdd
ArmanSattar <armanscsgo@gmail.com>
2020-02-25 19:30:59 +0000

    Fixed formatting issue

--- src/data.py
+++ src/data.py
@@ -1,20 +1,25 @@
-#imported library
 import random
 #set randomly named values to variables
-schools = ["Ockylnge", "Ratton", "Cavendish"]
-gender = ["Male", "Female"]
-courseType = ["A-Level", "Vocational", "Applied General"]
-collegeAttending = ["Eastbourne", "Hastings", "Lewes"]
+
+schools = ["SchoolName.ocklynge", "SchoolName.parklands"]
+gender = ["Gender.male", "Gender.female"]
+courseType = ["CourseType.alevel", "CourseType.vocational", "CourseType.appgeneral"]
+collegeAttending = ["College.eastbourne", "College.hastings", "College.lewes"]
 #opens a .ts file and then writes to it
-dataFile = open("randomData.ts", "w")
-dataFile.write("export const studentInfo:\nStudent[] = [\n")
+
+dataFile = open("src\RandomData.ts", "w")
+dataFile.write('import { CourseType, Filters, Gender, SchoolName, College } from "./enums"\n\nexport const studentInfo:\nStudent[] = [\n')
 #loops until condition is met and prints random data
-for currentNumber in range(1,10):
+
+for currentNumber in range(1,100):
     schoolsRandom = random.randint(1,len(schools))
     genderRandom = random.randint(1,len(gender))
     courseTypeGender = random.randint(1,len(courseType))
     collegeAttendingRandom = random.randint(1,len(collegeAttending))
-    dataFile.write("   {\n      school: " + schools[schoolsRandom - 1] + "," + "\n      college: " + collegeAttending[collegeAttendingRandom - 1] + "," + "\n      gender: " + gender[genderRandom - 1] + ","+ " \n      course: " + courseType[courseTypeGender - 1] + "," + "\n   },\n");dataFile.write("\n")
+    dataFile.write("   {\n      school: " + schools[schoolsRandom - 1] + "," + "\n      college: " + collegeAttending[collegeAttendingRandom - 1] + "," + "\n      gender: " + 
+    gender[genderRandom - 1] + ","+ " \n      course: " + courseType[courseTypeGender - 1] + "," + "\n   },\n");dataFile.write("\n")
+
 dataFile.write("]")
 #closes file
+
 dataFile.close()
