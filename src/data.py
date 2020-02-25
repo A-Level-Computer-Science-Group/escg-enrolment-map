f5ec313106998a0e9ca9514dc20e3586731a7f5d
ArmanSattar <armanscsgo@gmail.com>
2020-02-25 18:35:52 +0000

    added comments and amended some mistakes

--- src/data.py
+++ src/data.py
@@ -1,16 +1,20 @@
+#imported library
 import random
-
+#set randomly named values to variables
 schools = ["Ockylnge", "Ratton", "Cavendish"]
 gender = ["Male", "Female"]
-courseType = ["A-Level", "Vocational", "Special Needs"]
+courseType = ["A-Level", "Vocational", "Applied General"]
 collegeAttending = ["Eastbourne", "Hastings", "Lewes"]
+#opens a .ts file and then writes to it
 dataFile = open("randomData.ts", "w")
-
+dataFile.write("export const studentInfo:\nStudent[] = [\n")
+#loops until condition is met and prints random data
 for currentNumber in range(1,10):
     schoolsRandom = random.randint(1,len(schools))
     genderRandom = random.randint(1,len(gender))
     courseTypeGender = random.randint(1,len(courseType))
     collegeAttendingRandom = random.randint(1,len(collegeAttending))
-    dataFile.write("{\n   school: " + schools[schoolsRandom - 1] + "," + "\n   college: " + collegeAttending[collegeAttendingRandom - 1] + "," + "\n   gender: " + gender[genderRandom - 1] + ","+ " \n   course: " + courseType[courseTypeGender - 1] + "," + "\n},\n");dataFile.write("\n")
-    
+    dataFile.write("   {\n      school: " + schools[schoolsRandom - 1] + "," + "\n      college: " + collegeAttending[collegeAttendingRandom - 1] + "," + "\n      gender: " + gender[genderRandom - 1] + ","+ " \n      course: " + courseType[courseTypeGender - 1] + "," + "\n   },\n");dataFile.write("\n")
+dataFile.write("]")
+#closes file
 dataFile.close()
