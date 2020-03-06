b4565585061d212f905eac7472f4ed9ff6515be3
Ethan Brierley <ethanboxx@gmail.com>
2020-03-06 17:09:25 +0000

    Remove comments that broke config
    json does not support comments

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -16,9 +16,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    // Makes the bad front end work.
-    //TODO
-    //FIXME
     "no-use-before-define": "warn",
     "no-console": "warn",
     "no-use-before-define": "warn"
