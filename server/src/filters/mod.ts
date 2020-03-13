4a5340874cce3b7b27200097e55291b8a6258477
Ethan Brierley <ethanboxx@gmail.com>
2020-03-13 17:57:18 +0000

    Use string unions

--- server/src/filters/mod.ts
+++ server/src/filters/mod.ts
@@ -1,65 +1,49 @@
 import { students, Student } from '../data/mod';
 
+// const FilterFuncs = {
+//   /**
+//    * Returns what enum the Filter comes from.
+//    * @param filter A Filter.
+//    */
+//   filterType: function(
+//     filter: Filter,
+//   ): typeof GenderFilter | typeof CourseFilter {
+//     return [GenderFilter, CourseFilter].find(f => {
+//       return Object.values(f).includes(filter as any);
+//     });
+//   },
+
+//   /**
+//    * Returns a boolean, which is true if the filter value
+//    * is included in the Enumerator's values.
+//    * @param filter A Filter.
+//    * @param E An Enumerator.
+//    */
+//   isType: function(filter: Filter, E: Enumerator): boolean {
+//     return Object.values(E).includes(filter as any);
+//   },
+// };
+
 export type Filter = GenderFilter | CourseFilter;
 
-const FilterFuncs = {
-  /**
-   * Returns what enum the Filter comes from.
-   * @param filter A Filter.
-   */
-  filterType: function(
-    filter: Filter,
-  ): typeof GenderFilter | typeof CourseFilter {
-    return [GenderFilter, CourseFilter].find(f => {
-      return Object.values(f).includes(filter as any);
-    });
-  },
+export type GenderFilter = 'male' | 'female';
 
-  /**
-   * Returns a boolean, which is true if the filter value 
-   * is included in the Enumerator's values.
-   * @param filter A Filter.
-   * @param E An Enumerator.
-   */
-  isType: function(filter: Filter, E: Enumerator): boolean {
-    return Object.values(E).includes(filter as any);
-  },
-};
+export type CourseFilter = 'a-level' | 'vocational' | 'applied-general';
 
-export enum GenderFilter {
-  Male = 'male',
-  Female = 'female',
-}
-
-export enum CourseFilter {
-  alevel = 'a-level',
-  vocational = 'vocational',
-  appgeneral = 'applied-general',
-}
-
-/**
- * Returns enum variant that matches a value if that value can be constant-initialized into the enum.
- * If the variant check fails it throws an exception.
- * This creates a safe way to serialize data to an enum.
- * @param enumList A provider of variants list. The name of the enum type you want to create can be placed here.
- * @param value The value you want to constant-initialize to the enum type.
- */
-function convertToEnum<E, V>(enumList: { [s: string]: E }, value: V): E {
-  if (Object.values(enumList).includes(value as any))
-    return (value as unknown) as E;
-  throw `Invalid enumerator variant: "${value}". All valid variants are [${Object.values(
-    enumList,
-  ).toString()}].`;
+function isFilter(str: string): str is Filter {
+  return (
+    str == 'male' ||
+    str == 'female' ||
+    str == 'a-level' ||
+    str == 'vocational' ||
+    str == 'applied-general'
+  );
 }
 
 export function parseQueries(_gender?: string, _course?: string): Filter[] {
-  const course: CourseFilter[] =
-    _course != null
-      ? _course.split(',').map(x => convertToEnum(CourseFilter, x))
-      : [];
-  const gender: GenderFilter[] =
-    _gender != null ? [convertToEnum(GenderFilter, _gender)] : [];
-  return (course as Filter[]).concat(gender);
+  const course: string[] = _course != null ? _course.split(',') : [];
+  const gender: string[] = _gender != null ? [_gender] : [];
+  return course.concat(gender).filter(<(x) => x is Filter>(x => isFilter(x)));
 }
 
 export function applyFilters(filters: Filter[]): Student[] {
